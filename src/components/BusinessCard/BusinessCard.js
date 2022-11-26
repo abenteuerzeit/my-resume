b0d81e60a4c74286eb8df486af2c03e767a54d7d
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-26 15:11:58 +0100

    update cards look

--- src/components/BusinessCard/BusinessCard.js
+++ src/components/BusinessCard/BusinessCard.js
@@ -6,24 +6,27 @@ const BusinessCard = (props) => {
     return (
         <>
             <article>
-                <small>
-                    Polish-American philosopher, musician, and IT professional.
-                </small>
                 <address>
-                <ul>
-                    <li><a href="mailto:amroz.muzyka@gmail.com">amroz.muzyka@gmail.com</a></li>
-                    <li><a href="tel:+48-535-322-909">+48 535 322 909</a></li>
-                </ul>
+                    <dl>
+                        <dt>Tel.: </dt>
+                        <dd>
+                            <a href="tel:+48535322909">+48 535-322-909</a>
+                        </dd>
+                        <dt>E-mail: </dt>
+                        <dd>
+                            <a href="mailto:amroz.muzyka@gmail.com">amroz.muzyka@gmail.com</a>
+                        </dd>
+                        <dt>GitHub: </dt>
+                        <dd>
+                            {/* github icon */}
+                            <img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="GitHub" height='10px' className='icon'/>
+                            <a href="https://github.com/abenteuerzeit" target="_blank" rel="noreferrer">abenteuerzeit</a>
+                        </dd>
+                    </dl>
                 </address>
-                <dl>
-                    <dt>From</dt>
-                    <dd>Baltimore, MD, USA </dd>
-                    <dt>Living in</dt>
-                    <dd>Kraków, Poland</dd>
-                </dl>
 
-                <Link to='/'><button>Hide</button></Link>
             </article>
+            <Link to='/'><button>Hide</button></Link>
         </>
     )
 }
