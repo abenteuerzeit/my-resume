ff1c189f03c75cac101935e221224664d7e31dc5
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-26 11:58:48 +0100

    Implement pillbox navbar component design

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,12 @@ const Nav = (props) => {
     return (
         <nav>
             <ul>
-                <li><a href="/guitar-teacher">Guitar Teacher</a></li>
-                <li><a href="/english-teacher">English Teacher</a></li>
+                <li>
+                    <a href="/guitar-teacher">Guitar Teacher</a>
+                </li>
+                <li>
+                    <a href="/english-teacher">English Teacher</a>
+                </li>
                 <li><a href="/editor">Philosopher</a></li>
                 <li><a href="/guitar-teacher">Musician</a></li>
                 <li><a href="/guitar-teacher">IT & Web Development</a></li>
