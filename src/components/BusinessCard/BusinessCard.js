7b9e93b26178f99120459b049ae65e3ff0a7167c
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-28 01:41:44 +0100

    update businesscard look

--- src/components/BusinessCard/BusinessCard.js
+++ src/components/BusinessCard/BusinessCard.js
@@ -10,7 +10,8 @@ const BusinessCard = (props) => {
                     <dl>
                         <dt>Tel.: </dt>
                         <dd>
-                            <a href="tel:+48535322909">+48 535-322-909</a>
+                            <a href="tel:+48535322909">+48 535-322-909</a><br />
+                            <a href="tel:+14432169316">+1 (443) 216-93-16</a>
                         </dd>
                         <dt>E-mail: </dt>
                         <dd>
@@ -22,6 +23,32 @@ const BusinessCard = (props) => {
                             <img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="GitHub" height='10px' className='icon'/>
                             <a href="https://github.com/abenteuerzeit" target="_blank" rel="noreferrer">abenteuerzeit</a>
                         </dd>
+                        <dt>LinkedIn: </dt>
+                        <dd>
+                            {/* linkedin icon */}
+                            <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="LinkedIn" height='10px' className='icon'/>
+                            <a href="https://www.linkedin.com/in/abenteuerzeit" target="_blank" rel="noreferrer">Adrian Mróz</a>
+                        </dd>
+                        <dt>Facebook: </dt>
+                        <dd>
+                            {/* facebook icon */}
+                            <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" alt="Facebook" height='10px' className='icon'/>
+                            <a href="https://www.facebook.com/adrian.mroz/" target="_blank" rel="noreferrer">Adrian Mróz</a>
+                        </dd>
+                        <dt>Twitter</dt>
+                        <dd>
+                            {/* twitter icon */}
+                            <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="Twitter" height='10px' className='icon'/>
+                            <a href="https://twitter.com/AdrianMroz" target="_blank" rel="noreferrer">@AdrianMroz</a>
+                        </dd>
+                        <dt>Google Scholar</dt>
+                        <dd>
+                            {/* google scholar icon */}
+                            <img src="https://img.icons8.com/ios-filled/50/000000/google-scholar.png" alt="Google Scholar" height='10px' className='icon'/>
+                            <a href="https://scholar.google.com/citations?user=Z8Z9ZqMAAAAJ&hl=en" target="_blank" rel="noreferrer">Adrian Mróz</a>
+                        </dd>
+                        
+
                     </dl>
                 </address>
 
--- src/components/English/English.js
+++ src/components/English/English.js
@@ -27,17 +27,38 @@ const English = (props) => {
                         </li>
                     </ul>
                 </nav>
-                <h2>English Teacher</h2>
+                <h2>English Mentor</h2>
            </header>
            <main>
                 <section id="work-experience" className="cv-section">
-                    <h3>Professional History</h3>
-                    <p>
-                        Job Details
-                    </p>
+                    <h3>Work Experience</h3>
+                    <div>
+                        <h4>Certifications</h4>
+                        <ul>
+                            <li>2020: professional certification in teaching English online (CTEO), the 170h online teaching specialization course by ITTT (International TEFL and TESOL Training) was completed.</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4>English as a Second Language Teacher</h4>
+                        <div>
+                            <div>Period: 01-10-2019 &ndash; 30.06-2022</div>
+                            <div>
+                                <div>Employer: "Mały Rynek" Foreign Language School</div>
+                                <div>Location: Kraków, Poland and remotely</div>
+                                <div>Reference: Dorota Daszkiewicz </div>
+                                <div>Description: lorem ipsum dolor sit amet, consectetur adipis occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                                </div>
+                            </div>
+                        </div>
 
+                    </div>
                 </section>
-            
+                <section id="education" className="cv-section">
+                    <h3>Education</h3>
+                </section>
+                <section id="skills" className="cv-section">
+                    <h3>Skills</h3>
+                </section>
            </main>
         </>
     );
